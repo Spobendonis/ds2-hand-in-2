f0060f006b1785b83ee8a3d671f636188963afed
Tobias Gad Spoorendonk <tosp@itu.dk>
2022-09-21 16:03:38 +0200

    3 way handshake sort of completed

--- tcpsim.go
+++ tcpsim.go
@@ -2,8 +2,65 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+type Msg struct {
+	msg   string
+	order int
+}
+
+// var connection int = 0
+var msg = make(chan Msg)
+
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("Query a greeting from the server (0-6)")
+
+	port := make(chan int)
+	go client(port)
+	go server(port)
+	time.Sleep(10 * time.Second)
+}
+
+func client(port chan int) {
+	var cliPing int = 0
+	var serPing int = 0
+	var order int = 0
+	for {
+		var input string
+		fmt.Scanln(&input)
+		port <- cliPing
+		if <-port == cliPing+1 {
+			cliPing++
+			//fmt.Println("client: " + strconv.Itoa(cliPing))
+			if <-port == serPing {
+				serPing++
+				//fmt.Println("server: " + strconv.Itoa(serPing))
+				port <- serPing
+				//DATA TRANSFER
+				m := Msg{input, order}
+				msg <- m
+				order++
+			}
+		}
+	}
+}
+
+func server(port chan int) {
+	var cliPing int = 0
+	var serPing int = 0
+	for {
+		if <-port == cliPing {
+			cliPing++
+			//fmt.Println("client: " + strconv.Itoa(cliPing))
+			port <- cliPing
+			port <- serPing //Here it wouldnt work irl
+			if <-port == serPing+1 {
+				serPing++
+				//fmt.Println("server: " + strconv.Itoa(serPing))
+				//DATA TRANSFER
+				fmt.Println(<-msg)
+			}
+		}
+	}
 }
